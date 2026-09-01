7bbbfd286fcfbb118e40ceb66ad3f1a8e601134d
jingyuguantou-ui <jingyuguantou@gmail.com>
2026-09-01 18:52:49 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "pindou-v5";
+const CACHE = "pindou-v2";
 const ASSETS = [
   ".",
   "index.html",
@@ -6,20 +6,10 @@ const ASSETS = [
   "js/app.js",
   "js/palette.js",
   "js/mard-colors.js",
-  "js/library-db.js",
-  "js/tutorial.js",
-  "js/features.js",
-  "js/versions.js",
-  "js/shortcuts.js",
-  "js/error-boundary.js",
-  "js/ipad-fixes.js",
-  "js/tests.js",
   "icons/icon-192.png",
   "icons/icon-512.png",
   "icons/icon-maskable-512.png",
 ];
-
-let dynamicManifest = null;
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
@@ -41,30 +31,9 @@ self.addEventListener("activate", (e) => {
   );
 });
 
-self.addEventListener("message", (e) => {
-  if (e.data && e.data.type === "update-manifest") {
-    dynamicManifest = { theme_color: e.data.theme_color, background_color: e.data.background_color };
-  }
-});
-
 self.addEventListener("fetch", (e) => {
   const req = e.request;
   if (req.method !== "GET") return;
-  const url = new URL(req.url);
-  if (url.pathname.endsWith("manifest.webmanifest") && dynamicManifest) {
-    e.respondWith(
-      fetch(req).then((res) => {
-        return res.json().then((manifest) => {
-          manifest.theme_color = dynamicManifest.theme_color;
-          manifest.background_color = dynamicManifest.background_color;
-          return new Response(JSON.stringify(manifest), {
-            headers: { "Content-Type": "application/manifest+json" },
-          });
-        });
-      })
-    );
-    return;
-  }
   e.respondWith(
     fetch(req)
       .then((res) => {
